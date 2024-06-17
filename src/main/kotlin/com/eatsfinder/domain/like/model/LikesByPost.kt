9ece595b1541c00d89493988273a08ef860eda3a
rim109 <fla7722@naver.com>
2024-06-17 22:31:03 +0900

    ✨ Feat: 유저 게시물 좋아요 엔티티 작성(#1)

---
+++ src/main/kotlin/com/eatsfinder/domain/like/model/LikesByPost.kt
@@ -0,0 +1,27 @@
+package com.eatsfinder.domain.like.model
+
+import com.eatsfinder.domain.post.model.Post
+import com.eatsfinder.domain.user.model.User
+import jakarta.persistence.*
+import org.hibernate.annotations.OnDelete
+import org.hibernate.annotations.OnDeleteAction
+
+@Entity
+@Table(name = "likes_by_posts")
+class LikesByPost(
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.NO_ACTION)
+    val userId: User,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    val postId: Post
+
+) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null
+}
