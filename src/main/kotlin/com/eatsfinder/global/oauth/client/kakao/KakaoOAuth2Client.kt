31038af636a3963938d3a1a1264d5637f233c5cf
rim109 <fla7722@naver.com>
2024-06-20 19:26:59 +0900

    ✨ Feat: 카카오 client 틀 작성 (#5)

---
+++ src/main/kotlin/com/eatsfinder/global/oauth/client/kakao/KakaoOAuth2Client.kt
@@ -0,0 +1,28 @@
+package com.eatsfinder.global.oauth.client.kakao
+
+import com.eatsfinder.domain.user.dto.OAuth2UserInfo
+import com.eatsfinder.domain.user.model.SocialType
+import com.eatsfinder.global.oauth.client.OAuth2Client
+import org.springframework.stereotype.Component
+import org.springframework.web.client.RestClient
+
+@Component
+class KakaoOAuth2Client(
+    private val restClient: RestClient
+) : OAuth2Client {
+    override fun generateLoginPageUrl(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun getAccessToken(authorizationCode: String): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun retrieveUserInfo(accessToken: String): OAuth2UserInfo {
+        TODO("Not yet implemented")
+    }
+
+    override fun supports(provider: SocialType): Boolean {
+        TODO("Not yet implemented")
+    }
+}
