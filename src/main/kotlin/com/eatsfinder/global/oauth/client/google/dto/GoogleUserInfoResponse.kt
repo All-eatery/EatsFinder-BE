2d407f2df6ed0b7097ab04e9c1b69e55864e7347
rim109 <fla7722@naver.com>
2024-06-20 22:54:08 +0900

    ✨ Feat: 구글 소셜로그인 dto 작성 (#5)

---
+++ src/main/kotlin/com/eatsfinder/global/oauth/client/google/dto/GoogleUserInfoResponse.kt
@@ -0,0 +1,22 @@
+package com.eatsfinder.global.oauth.client.google.dto
+
+import com.eatsfinder.domain.user.dto.OAuth2UserInfo
+import com.eatsfinder.domain.user.model.SocialType
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.databind.annotation.JsonNaming
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
+class GoogleUserInfoResponse(
+    id: String,
+    name: String,
+
+    @JsonProperty
+    email: String
+
+) : OAuth2UserInfo(
+    provider = SocialType.GOOGLE,
+    id = id,
+    nickname = name,
+    email = email
+)
