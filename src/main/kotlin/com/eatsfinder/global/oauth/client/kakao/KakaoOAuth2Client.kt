03a1706cb11144613b4e1bbab4bcf7606aa89655
rim109 <fla7722@naver.com>
2024-06-22 16:45:08 +0900

    ✨ Feat: client 작성 (#5)
    - 카카오 client 작성
    - 구글 client 작성

--- src/main/kotlin/com/eatsfinder/global/oauth/client/kakao/KakaoOAuth2Client.kt
+++ src/main/kotlin/com/eatsfinder/global/oauth/client/kakao/KakaoOAuth2Client.kt
@@ -3,26 +3,59 @@ package com.eatsfinder.global.oauth.client.kakao
 import com.eatsfinder.domain.user.dto.OAuth2UserInfo
 import com.eatsfinder.domain.user.model.SocialType
 import com.eatsfinder.global.oauth.client.OAuth2Client
+import com.eatsfinder.global.oauth.client.kakao.dto.KakaoTokenResponse
+import com.eatsfinder.global.oauth.client.kakao.dto.KakaoUserInfoResponse
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
+import org.springframework.util.LinkedMultiValueMap
 import org.springframework.web.client.RestClient
+import org.springframework.web.client.body
 
 @Component
 class KakaoOAuth2Client(
-    private val restClient: RestClient
+    private val restClient: RestClient,
+    @Value("\${oauth2.kakao.client_id}") val clientId: String,
+    @Value("\${oauth2.kakao.redirect_url}") val redirectUrl: String,
+    @Value("\${oauth2.kakao.auth_server_base_url}") val authServerBaseUrl: String,
+    @Value("\${oauth2.kakao.resource_server_base_url}") val resourceServerBaseUrl: String
 ) : OAuth2Client {
     override fun generateLoginPageUrl(): String {
-        TODO("Not yet implemented")
+        return StringBuilder(authServerBaseUrl)
+            .append("/oauth/authorize")
+            .append("?client_id=").append(clientId)
+            .append("&redirect_uri=").append(redirectUrl)
+            .append("&response_type=").append("code")
+            .toString()
     }
 
     override fun getAccessToken(authorizationCode: String): String {
-        TODO("Not yet implemented")
+        val requestData = mutableMapOf(
+            "grant_type" to "authorization_code",
+            "client_id" to clientId,
+            "code" to authorizationCode,
+            "redirect_uri" to redirectUrl
+        )
+        return restClient.post()
+            .uri("$authServerBaseUrl/oauth/token")
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .body(LinkedMultiValueMap<String, String>().apply { this.setAll(requestData) })
+            .retrieve()
+            .body<KakaoTokenResponse>()
+            ?.accessToken
+            ?: throw RuntimeException("유저 조회 실패")
     }
 
     override fun retrieveUserInfo(accessToken: String): OAuth2UserInfo {
-        TODO("Not yet implemented")
+        return restClient.get()
+            .uri("$resourceServerBaseUrl/v2/user/me")
+            .header("Authorization", "Bearer $accessToken")
+            .retrieve()
+            .body<KakaoUserInfoResponse>()
+            ?: throw RuntimeException("유저 정보 조회 실패")
     }
 
     override fun supports(provider: SocialType): Boolean {
-        TODO("Not yet implemented")
+        return provider == SocialType.KAKAO
     }
 }
