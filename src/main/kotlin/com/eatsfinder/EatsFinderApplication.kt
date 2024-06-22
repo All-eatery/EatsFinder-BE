e74db9850a4ee9ce374519d8a925fd866f52a6c8
rim109 <fla7722@naver.com>
2024-06-22 21:38:48 +0900

    🐛 Fix:	 @EnableJpaRepositories 추가 (#5)

--- src/main/kotlin/com/eatsfinder/EatsFinderApplication.kt
+++ src/main/kotlin/com/eatsfinder/EatsFinderApplication.kt
@@ -3,7 +3,9 @@ package com.eatsfinder
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
+@EnableJpaRepositories
 @EnableJpaAuditing
 @SpringBootApplication
 class EatsFinderApplication
