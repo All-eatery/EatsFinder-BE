ff6780e5064ab33baaec2b887aa3eb10da08ab9f
rim109 <fla7722@naver.com>
2024-06-22 18:53:29 +0900

    ✒️ Rename: 클래스명 수정 및 service 어노테이션 추가 (#5)

--- src/main/kotlin/com/eatsfinder/domain/user/service/OAuth2LoginService.kt
+++ src/main/kotlin/com/eatsfinder/domain/user/service/OAuth2LoginService.kt
@@ -3,8 +3,10 @@ package com.eatsfinder.domain.user.service
 import com.eatsfinder.domain.user.model.SocialType
 import com.eatsfinder.global.oauth.client.OAuth2ClientService
 import com.eatsfinder.global.security.jwt.JwtPlugin
+import org.springframework.stereotype.Service
 
-class OAuthLoginService(
+@Service
+class OAuth2LoginService(
     private val oAuth2ClientService: OAuth2ClientService,
     private val oAuthUserService: OAuthUserService,
     private val jwtPlugin: JwtPlugin
