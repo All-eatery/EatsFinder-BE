a199fd3707999f55f4aa6ffdd8a7d81d94b37acc
rim109 <fla7722@naver.com>
2024-06-22 18:02:56 +0900

    ✨ Feat: OAuthLoginService 작성 (#5)

---
+++ src/main/kotlin/com/eatsfinder/domain/user/service/OAuthLoginService.kt
@@ -0,0 +1,17 @@
+package com.eatsfinder.domain.user.service
+
+import com.eatsfinder.domain.user.model.SocialType
+import com.eatsfinder.global.oauth.client.OAuth2ClientService
+import com.eatsfinder.global.security.jwt.JwtPlugin
+
+class OAuthLoginService(
+    private val oAuth2ClientService: OAuth2ClientService,
+    private val oAuthUserService: OAuthUserService,
+    private val jwtPlugin: JwtPlugin
+) {
+    fun login(provider: SocialType, authorizationCode: String): String {
+        return oAuth2ClientService.getUserInfo(provider, authorizationCode)
+            .let { oAuthUserService.registerIfNotExist(it) }
+            .let { jwtPlugin.generateAccessToken(it.id!!.toString(), it.email, it.role.toString()) }
+    }
+}
