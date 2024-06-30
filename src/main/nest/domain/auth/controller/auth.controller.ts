bdc70bbe92b6d7c376638b58ba5a139e870bd8c9
go-tiger <dev.gotiger@gmail.com>
2024-07-01 01:19:01 +0900

    ✨ Feat: Swagger 문서에 적용(#29)

--- src/main/nest/domain/auth/controller/auth.controller.ts
+++ src/main/nest/domain/auth/controller/auth.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Body, Post } from '@nestjs/common';
 import { AuthService } from '../service/auth.service';
 import { CreateUserDto } from '../dto/create-user.dto';
+import { ApiTags, ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signup')
+  @ApiOperation({ summary: '로컬 회원 가입' })
+  @ApiBody({ type: CreateUserDto })
+  @ApiResponse({ status: 201, description: '회원 가입 성공' })
   async createUser(@Body() dto: CreateUserDto) {
     return await this.authService.createUser(dto);
   }
--- src/main/nest/domain/auth/dto/create-user.dto.ts
+++ src/main/nest/domain/auth/dto/create-user.dto.ts
@@ -1,7 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty()
   name: string;
+
+  @ApiProperty()
   phoneNumber: string;
+
+  @ApiProperty()
   email: string;
+
+  @ApiProperty()
   password: string;
+
+  @ApiProperty()
   nickname: string;
 }
