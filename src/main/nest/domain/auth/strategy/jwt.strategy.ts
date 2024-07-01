3351dcd6a5bc9cd110d3e19f0d02caa8e326f2af
go-tiger <dev.gotiger@gmail.com>
2024-07-01 20:23:38 +0900

    🍱 Add: jwt, local 전략(#29)

---
+++ src/main/nest/domain/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { AuthService } from '../service/auth.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly configService: ConfigService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: configService.get<string>('ENV_JWT_SECRET'),
+    });
+  }
+}
