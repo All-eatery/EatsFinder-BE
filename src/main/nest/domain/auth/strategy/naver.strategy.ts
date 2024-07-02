b1851265ca13b121e26124d4b38e058bb8e1b8e6
go-tiger <dev.gotiger@gmail.com>
2024-07-03 00:09:05 +0900

    🍱 Add: naver 전략(#5)

---
+++ src/main/nest/domain/auth/strategy/naver.strategy.ts
@@ -0,0 +1,39 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'n-passport-naver';
+import { Injectable } from '@nestjs/common';
+import { AuthService } from '../service/auth.service';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class NaverStrategy extends PassportStrategy(Strategy, 'naver') {
+  constructor(
+    private authService: AuthService,
+    private readonly configService: ConfigService,
+  ) {
+    super({
+      clientID: configService.get<string>('ENV_NAVER_CLIENT_ID'),
+      clientSecret: configService.get<string>('ENV_NAVER_CLIENT_SECRET'),
+      callbackURL: configService.get<string>('ENV_NAVER_REDIRECT_URI'),
+    });
+  }
+
+  async validate(accessToken: string, refreshToken: string, profile: any, done: any): Promise<any> {
+    const { provider, _json } = profile;
+
+    const userData = {
+      name: _json.name,
+      phoneNumber: _json.mobile_e164,
+      email: _json.email,
+      password: _json.id,
+      nickname: _json.nickname,
+      profileImage: _json.profile_image,
+    };
+
+    const user = await this.authService.validateUser(_json.email, _json.id, provider.toUpperCase());
+
+    if (!user) {
+      return await this.authService.createUser(userData, provider.toUpperCase());
+    }
+    return user;
+  }
+}
