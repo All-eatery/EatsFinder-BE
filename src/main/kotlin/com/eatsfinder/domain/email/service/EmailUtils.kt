3e15959fa26818eb5af35933f0ae6d285e52a768
rim109 <fla7722@naver.com>
2024-07-05 02:41:22 +0900

    🍱 Add: EmailUtils 에서 이메일 보내는 양식 임시로 추가 (#39)

--- src/main/kotlin/com/eatsfinder/domain/email/service/EmailUtils.kt
+++ src/main/kotlin/com/eatsfinder/domain/email/service/EmailUtils.kt
@@ -1,6 +1,7 @@
 package com.eatsfinder.domain.email.service
 
 import jakarta.mail.MessagingException
+import jakarta.mail.internet.MimeMessage
 import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.mail.javamail.MimeMessageHelper
 import org.springframework.stereotype.Component
@@ -15,8 +16,29 @@ class EmailUtils(
         val helper = MimeMessageHelper(message, true)
         try {
             helper.setTo(email)
-            helper.setSubject("EatsFinder의 이메일 인증번호입니다!")
-            helper.setText(authCode)
+            message.setRecipients(MimeMessage.RecipientType.TO, email)
+            helper.setSubject("[EatsFinder] 요청하신 이메일 인증번호 안내 메일입니다")
+            var body = ""
+            body += (" <div" 																																																	+
+                    "	style=\"font-family: 'Apple SD Gothic Neo', 'sans-serif' !important; width: 460px; height: 600px; border-top: 4px solid #f89b00; margin: 100px auto; padding: 30px 0; box-sizing: border-box; color: black;\"> "		+
+                    "	<h1 style=\"margin: 0; padding: 0 5px; font-size: 28px; font-weight: 400; color: black;\">"																															+
+                    "		<span style=\"font-size: 15px; margin: 0 0 10px 3px; color: black;\">EatsFinder</span><br />"																													+
+                    "		<span style=\"color: #f89b00\">메일인증</span> 안내입니다.<br />"																																				+
+                    "	</h1>\n"																																																+
+                    "	<p style=\"font-size: 16px; line-height: 26px; margin-top: 50px; padding: 0 5px; color: black;\">"																							+
+                    "		안녕하세요.<br />"																																													+
+                    "		EatsFinder에 가입해 주셔서 진심으로 감사드립니다.<br />"																																						+
+                    "		아래 <b style=\"color: #f89b00\">'인증 번호'</b> 을 입력하여 회원가입을 완료해 주세요.<br />"																													+
+                    "		참고로 본 보안 코드는 <b style=\"color: #f89b00\">5분 후</b>에 만료됩니다!</br>\t\t감사합니다</br>\t"																																															+
+                    "	</p>"																																																	+
+                    "		<p"																																																	+
+                    "			style=\"display: inline-block; width: 210px; height: 45px; margin: 30px 5px 40px; background: #f89b00; line-height: 45px; vertical-align: middle; font-size: 16px; color: #FFF; text-decoration: none; text-align: center;\">"							+
+                    "			${authCode}</p>"																																														+
+                    "	</a>"																																																	+
+                    "	<div style=\"border-top: 1px solid #DDD; padding: 5px;\"></div>"																																		+
+                    " </div>"
+                    )
+            message.setText(body, "UTF-8", "html")
 
         } catch (e: MessagingException) {
             e.printStackTrace()
