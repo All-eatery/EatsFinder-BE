b117ba9efc83130dde716f3ca2c35469a0a1f59f
go-tiger <dev.gotiger@gmail.com>
2024-07-10 05:16:59 +0900

    🍱 Add: 커스텀 데코레이터 추가(#49) - 커스텀 데코레이터 추가로 데코레이터 정리

--- src/main/nest/domain/post/controller/post.controller.ts
+++ src/main/nest/domain/post/controller/post.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Post, UploadedFile, UploadedFiles, UseInterceptors } from '@nestjs/common';
-import { ApiBody, ApiConsumes, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Controller, Post, UploadedFiles } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { PostService } from '../service/post.service';
-import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+import { ApiImageFiles } from '../../../global/decorator/files.decorator';
 
 @ApiTags('Post')
 @Controller('posts')
@@ -11,23 +11,7 @@ export class PostController {
   //Todo 임시 이미지 업로드 API 기능 구현
   @Post('image')
   @ApiOperation({ summary: '이미지 업로드(임시 구현)' })
-  @UseInterceptors(FilesInterceptor('files', 5))
-  @ApiConsumes('multipart/form-data')
-  @ApiBody({
-    description: '이미지 업로드',
-    schema: {
-      type: 'object',
-      properties: {
-        files: {
-          type: 'array',
-          items: {
-            type: 'string',
-            format: 'binary',
-          },
-        },
-      },
-    },
-  })
+  @ApiImageFiles('files')
   async saveImage(@UploadedFiles() files: Array<Express.Multer.File>) {
     return await this.postService.imageUpload(files);
   }
