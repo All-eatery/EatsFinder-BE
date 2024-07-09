d339557a7a0c66f15f7179e156ba82acf637a95b
go-tiger <dev.gotiger@gmail.com>
2024-07-09 23:57:22 +0900

    ✨ Feat: CORS 세팅(#52) - 모든 origin 허용

--- src/main/nest/main.ts
+++ src/main/nest/main.ts
@@ -9,6 +9,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
+  app.enableCors({
+    origin: true,
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  });
+
   app.useGlobalInterceptors(new BigIntInterceptor());
 
   const prismaService = app.get(PrismaService);
