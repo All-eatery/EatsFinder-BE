05d09ed0bfcb3a44ff86cafe75e50ad9a01a8ed8
go-tiger <dev.gotiger@gmail.com>
2024-07-11 08:42:14 +0900

    ✨ Feat: 장소 등록(#59)

--- src/main/nest/domain/place/controller/place.controller.ts
+++ src/main/nest/domain/place/controller/place.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { PlaceService } from '../service/place.service';
+import { Places } from '@prisma/client';
+import { ApiBody, ApiConflictResponse, ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { CreatePlaceRequestDto, CreatePlaceResponseDto } from '../../../global/dto';
 
-@Controller('place')
+@ApiTags('Place')
+@Controller('places')
 export class PlaceController {
   constructor(private readonly placeService: PlaceService) {}
+
+  @Post()
+  @ApiOperation({ summary: '새로운 맛집 등록' })
+  @ApiCreatedResponse({ description: '맛집이 등록되었습니다.', type: CreatePlaceResponseDto })
+  @ApiConflictResponse({ description: '입력하신 장소가 이미 존재합니다.' })
+  @ApiBody({ type: CreatePlaceRequestDto })
+  async createPlace(@Body() dto: CreatePlaceRequestDto) {
+    const place: Places = await this.placeService.createPlace(dto);
+    return { message: '맛집이 등록되었습니다.', id: Number(place.id) };
+  }
 }
--- src/main/nest/domain/place/service/place.service.ts
+++ src/main/nest/domain/place/service/place.service.ts
@@ -1,4 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { PrismaService } from '../../../global/prisma/prisma.service';
+import { Categories } from '@prisma/client';
+import { CreatePlaceRequestDto } from '../../../global/dto';
 
 @Injectable()
-export class PlaceService {}
+export class PlaceService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async createPlace(dto: CreatePlaceRequestDto) {
+    const { name, address, roadAddress, telephone, x, y, category, categoryName, categoryCode } = dto;
+
+    const places = await this.prismaService.places.findMany({ where: { name, address } });
+    const isPlaceData = places.length === 0 ? false : places;
+    if (isPlaceData) throw new ConflictException('입력하신 장소가 이미 존재합니다.');
+
+    let isCategoryData = await this.prismaService.categories.findFirst({
+      where: { name: category.split('>')[1].trim() },
+    });
+    let createCategory: Categories;
+    if (!isCategoryData) {
+      createCategory = await this.prismaService.categories.create({
+        data: {
+          name: category.split('>')[1].trim(),
+          type: categoryName,
+          code: categoryCode,
+        },
+      });
+      isCategoryData = createCategory;
+    }
+
+    return await this.prismaService.places.create({
+      data: {
+        name,
+        address,
+        roadAddress,
+        telephone,
+        x,
+        y,
+        categoryId: isCategoryData.id,
+      },
+    });
+  }
+}
