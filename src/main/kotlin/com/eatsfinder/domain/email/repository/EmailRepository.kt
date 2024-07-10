8666d68d9da61ac164a4bcab85bd952f062c84d1
rim109 <fla7722@naver.com>
2024-07-10 23:50:55 +0900

    🐛 Fix: 이메일 인증 동시성 문제위해 락 도입(#50)

--- src/main/kotlin/com/eatsfinder/domain/email/repository/EmailRepository.kt
+++ src/main/kotlin/com/eatsfinder/domain/email/repository/EmailRepository.kt
@@ -1,10 +1,16 @@
 package com.eatsfinder.domain.email.repository
 
 import com.eatsfinder.domain.email.model.Email
+import jakarta.persistence.LockModeType
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Lock
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 interface EmailRepository : JpaRepository<Email, Long> {
-    fun findByEmail(email: String): Email?
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT e FROM Email e WHERE e.email = :email")
+    fun findByEmail(@Param("email") email: String): Email?
 
     fun findByCode(code: String): Email?
 
