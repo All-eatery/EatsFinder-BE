e9652d0c13d2dd7b0bab5eaa8c82a96c2b0bc905
go-tiger <dev.gotiger@gmail.com>
2024-07-12 00:01:13 +0900

    ✨ Feat: 메뉴 등록(#59)

--- src/main/nest/domain/place/menu/controller/menu.controller.ts
+++ src/main/nest/domain/place/menu/controller/menu.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { MenuService } from '../service/menu.service';
+import { PlaceMenus } from '@prisma/client';
+import { ApiBody, ApiConflictResponse, ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { CreateMenuRequestDto, CreateMenuResponseDto } from '../../../../global/dto';
 
-@Controller('menu')
+@ApiTags('Menu')
+@Controller('menus')
 export class MenuController {
   constructor(private readonly menuService: MenuService) {}
+
+  @Post()
+  @ApiOperation({ summary: '맛집에 메뉴 등록' })
+  @ApiCreatedResponse({ type: CreateMenuResponseDto })
+  @ApiConflictResponse({ description: '이미 메뉴가 등록되어 있습니다.' })
+  @ApiBody({ type: CreateMenuRequestDto })
+  async createPlace(@Body() dto: CreateMenuRequestDto) {
+    const { id, menu, placeId }: PlaceMenus = await this.menuService.createMenu(dto);
+    return { id: Number(id), menu: menu, placeId: Number(placeId) };
+  }
 }
--- src/main/nest/domain/place/menu/service/menu.service.ts
+++ src/main/nest/domain/place/menu/service/menu.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { PrismaService } from '../../../../global/prisma/prisma.service';
+import { CreateMenuRequestDto } from '../../../../global/dto';
 
 @Injectable()
-export class MenuService {}
+export class MenuService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async createMenu(dto: CreateMenuRequestDto) {
+    const { menu, placeId } = dto;
+
+    const menus = await this.prismaService.placeMenus.findMany({ where: { menu, placeId } });
+    const isMenuData = menus.length === 0 ? false : menus;
+    if (isMenuData) throw new ConflictException('이미 메뉴가 등록되어 있습니다.');
+
+    return await this.prismaService.placeMenus.create({
+      data: {
+        menu,
+        placeId,
+      },
+    });
+  }
+}
