411eb290f6edfdc9789ca255e9112b052021846f
go-tiger <dev.gotiger@gmail.com>
2024-07-11 21:14:23 +0900

    🍱 Add: 맛집 조회에 필요한 DTO 추가(#59)

--- src/main/nest/global/dto/index.ts
+++ src/main/nest/global/dto/index.ts
@@ -1,2 +1,6 @@
+// Response
 export * from './response/create-place-response.dto';
+export * from './response/find-place-response.dto';
+
+// Request
 export * from './request/create-place-request.dto';
