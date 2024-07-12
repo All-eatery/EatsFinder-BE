22fe95be6357bc1d554c0dd75e5fce674988e86f
go-tiger <dev.gotiger@gmail.com>
2024-07-13 08:14:45 +0900

    ✒️ Rename: 커스텀 데코레이터 수정(#49)

--- src/main/nest/domain/post/controller/post.controller.ts
+++ src/main/nest/domain/post/controller/post.controller.ts
@@ -1,7 +1,7 @@
 import { BadRequestException, Controller, NotFoundException, Post, UploadedFiles } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { PostService } from '../service/post.service';
-import { ApiImageFiles } from '../../../global/decorator/files.decorator';
+import { ApiImageFilesWithDto } from '../../../global/decorator/api-files.decorator';
 
 @ApiTags('Post')
 @Controller('posts')
@@ -11,7 +11,7 @@ export class PostController {
   //Todo 임시 이미지 업로드 API 기능 구현
   @Post('image')
   @ApiOperation({ summary: '이미지 업로드(임시 구현)' })
-  @ApiImageFiles('files')
+  @ApiImageFilesWithDto()
   async saveImage(@UploadedFiles() files: Array<Express.Multer.File>) {
     if (files.length === 0) throw new NotFoundException('필수 이미지가 없습니다.');
     if (files.length > 5) throw new BadRequestException('최대 5개까지 업로드 가능합니다.');
---
+++ src/main/nest/global/decorator/api-files.decorator.ts
@@ -0,0 +1,12 @@
+import { applyDecorators, UseInterceptors } from '@nestjs/common';
+import { AnyFilesInterceptor } from '@nestjs/platform-express';
+import { ApiBody, ApiConsumes } from '@nestjs/swagger';
+import { CreatePostRequestDto } from '../dto';
+
+export function ApiImageFilesWithDto() {
+  return applyDecorators(
+    UseInterceptors(AnyFilesInterceptor()),
+    ApiConsumes('multipart/form-data'),
+    ApiBody({ type: CreatePostRequestDto }),
+  );
+}
