1af2bba29e26144ba43813f94374f60b0546851e
rim109 <fla7722@naver.com>
2024-07-26 23:24:47 +0900

    🐛 Fix:	loginRedirectUrl 추가 (#72)

--- src/main/kotlin/com/eatsfinder/domain/user/controller/OAuth2LoginController.kt
+++ src/main/kotlin/com/eatsfinder/domain/user/controller/OAuth2LoginController.kt
@@ -6,6 +6,7 @@ import com.eatsfinder.domain.user.service.OAuth2LoginService
 import com.eatsfinder.global.oauth.client.OAuth2ClientService
 import io.swagger.v3.oas.annotations.Operation
 import jakarta.servlet.http.HttpServletResponse
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
@@ -14,13 +15,17 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpHeaders
+import java.net.URI
 
 @RestController
 @RequestMapping("/auth")
 class OAuth2LoginController(
+    @Value("\${frontend.domain}") private val loginRedirectUrl: String,
     private val oAuth2LoginService: OAuth2LoginService,
     private val oAuth2Client: OAuth2ClientService
 ) {
+    private val localHost = loginRedirectUrl
 
     @Operation(summary = "소셜 로그인 (로그인 페이지로 Redirect 하기)")
     @PreAuthorize("isAnonymous()")
@@ -41,8 +46,12 @@ class OAuth2LoginController(
     ): ResponseEntity<OAuthResponse> {
 
         val accessToken = oAuth2LoginService.login(provider, authorizationCode)
+
+        val headers = HttpHeaders()
+            .also { it.location = URI.create(localHost) }
+
         val oauthResponse = OAuthResponse(accessToken)
 
-        return ResponseEntity(oauthResponse, HttpStatus.OK)
+        return ResponseEntity(oauthResponse, headers, HttpStatus.PERMANENT_REDIRECT)
     }
 }
