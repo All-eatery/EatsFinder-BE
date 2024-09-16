74cd56b2b87cc10fe83bc2ed987088c2fa068a53
rim109 <fla7722@naver.com>
2024-09-16 21:48:42 +0900

    🐛 Fix:	res 수정(#96)
    - 게시글, 댓글 좋아요는 각각 게시글, 댓글이 삭제되면 조회안되도록 수정

--- src/main/kotlin/com/eatsfinder/domain/user/dto/user/active/MyActiveResponse.kt
+++ src/main/kotlin/com/eatsfinder/domain/user/dto/user/active/MyActiveResponse.kt
@@ -13,20 +13,26 @@ data class MyActiveResponse(
             val createdDate = DateTimeFormatter.ofPattern("yyyy.MM.dd")
             val data = log.map { logs ->
                 when (logs.myActiveType) {
-                    MyActiveType.POST_LIKES -> MyActiveDataResponse(
-                        type = "POST_LIKES",
-                        postLike = MyActivePostLikeResponse(
-                            postId = logs.postLikeId?.postId?.id,
-                            createdBy = MyActivePostUserResponse(
-                                postUserNickname = logs.postLikeId?.postId?.userId?.nickname,
-                                postImageUrl = logs.postLikeId?.postId?.thumbnailUrl
-                            ),
-                            postContent = logs.postLikeId?.postId?.content
-                        ),
-                        commentLike = null,
-                        comment = null,
-                        createdAt = logs.createdAt.toLocalDate().format(createdDate)
-                    )
+                    MyActiveType.POST_LIKES ->
+                        if (logs.postLikeId?.postId?.deletedAt == null) {
+                            MyActiveDataResponse(
+                                type = "POST_LIKES",
+                                postLike = MyActivePostLikeResponse(
+                                    postId = logs.postLikeId?.postId?.id,
+                                    createdBy = MyActivePostUserResponse(
+                                        postUserNickname = logs.postLikeId?.postId?.userId?.nickname,
+                                        postImageUrl = logs.postLikeId?.postId?.thumbnailUrl
+                                    ),
+                                    postContent = logs.postLikeId?.postId?.content
+                                ),
+                                commentLike = null,
+                                comment = null,
+                                createdAt = logs.createdAt.toLocalDate().format(createdDate)
+                            )
+                        } else {
+                            null
+                        }
+
                     MyActiveType.COMMENT -> MyActiveDataResponse(
                         type = "COMMENT",
                         postLike = null,
@@ -43,23 +49,29 @@ data class MyActiveResponse(
                         ),
                         createdAt = logs.createdAt.toLocalDate().format(createdDate)
                     )
-                    MyActiveType.COMMENT_LIKES -> MyActiveDataResponse(
-                        type = "COMMENT_LIKES",
-                        postLike = null,
-                        commentLike = MyActiveCommentLikeResponse(
-                            postId = logs.commentLikeId?.commentId?.postId?.id,
-                            commentId = logs.commentLikeId?.commentId?.id,
-                            createdBy = MyActiveCommentUserResponse(
-                                commentUserNickname = logs.commentLikeId?.commentId?.userId?.nickname,
-                                commentUserImageUrl = logs.commentLikeId?.commentId?.userId?.profileImage
-                            ),
-                            commentContent = logs.commentLikeId?.commentId?.content
-                        ),
-                        comment = null,
-                        createdAt = logs.createdAt.toLocalDate().format(createdDate)
-                    )
+
+                    MyActiveType.COMMENT_LIKES ->
+                        if (logs.commentLikeId?.commentId?.deletedAt == null) {
+                            MyActiveDataResponse(
+                                type = "COMMENT_LIKES",
+                                postLike = null,
+                                commentLike = MyActiveCommentLikeResponse(
+                                    postId = logs.commentLikeId?.commentId?.postId?.id,
+                                    commentId = logs.commentLikeId?.commentId?.id,
+                                    createdBy = MyActiveCommentUserResponse(
+                                        commentUserNickname = logs.commentLikeId?.commentId?.userId?.nickname,
+                                        commentUserImageUrl = logs.commentLikeId?.commentId?.userId?.profileImage
+                                    ),
+                                    commentContent = logs.commentLikeId?.commentId?.content
+                                ),
+                                comment = null,
+                                createdAt = logs.createdAt.toLocalDate().format(createdDate)
+                            )
+                        } else {
+                            null
+                        }
                 }
-            }
+            }.filterNotNull()
             return MyActiveResponse(data = data)
         }
     }
