d7ca7df74ac978c0e63c4462e0e92771e3e9a975
go-tiger <dev.gotiger@gmail.com>
2024-11-16 23:58:00 +0900

    ✨ Feat: 리스트 생성 API(#118)

--- src/main/nest/domain/bookmark/controller/bookmark.controller.ts
+++ src/main/nest/domain/bookmark/controller/bookmark.controller.ts
@@ -1,15 +1,23 @@
-import { Controller, Get, Post, Patch, Param, Delete } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Post, Patch, Param, Delete, Body } from '@nestjs/common';
+import { ApiBadRequestResponse, ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { BookmarkService } from '../service/bookmark.service';
+import { ApiGuard, GetUserId } from '../../../global/decorator';
+import { CreateBookmarkListRequestDto } from '../../../global/dto';
+import { Bookmarks } from '@prisma/client';
 
 @ApiTags('Bookmark')
 @Controller('bookmarks')
 export class BookmarkController {
   constructor(private readonly bookmarkService: BookmarkService) {}
 
-  @Post()
-  create() {
-    return this.bookmarkService.create();
+  @Post('lists')
+  @ApiGuard()
+  @ApiOperation({ summary: '리스트 생성' })
+  @ApiCreatedResponse({ description: '리스트가 생성되었습니다.' })
+  @ApiBadRequestResponse({ description: '생성할 수 있는 최대 리스트 개수는 100개까지입니다.' })
+  async create(@GetUserId() userId: number, @Body() dto: CreateBookmarkListRequestDto) {
+    const bookmark: Bookmarks = await this.bookmarkService.create(userId, dto);
+    return { message: '리스트가 생성되었습니다.', bookmark };
   }
 
   @Get()
--- src/main/nest/domain/bookmark/service/bookmark.service.ts
+++ src/main/nest/domain/bookmark/service/bookmark.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { CreateBookmarkListRequestDto } from '../../../global/dto';
+import { PrismaService } from '../../../global/prisma/prisma.service';
 
 @Injectable()
 export class BookmarkService {
-  create() {
-    return 'This action adds a new bookmark';
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(userId: number, dto: CreateBookmarkListRequestDto) {
+    const totalBookmark = await this.prismaService.bookmarks.count({ where: { userId } });
+    if (totalBookmark > 99) throw new BadRequestException('생성할 수 있는 최대 리스트 개수는 100개까지입니다.');
+    return await this.prismaService.bookmarks.create({ data: { title: dto.listname, userId } });
   }
 
   findAll() {
