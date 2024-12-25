4c960719aeec14416fc3b140016dae150dc61e37
rim109 <fla7722@naver.com>
2024-12-25 20:54:15 +0900

    🍱 Add: 검색 API Response companion object 추가  (#117)

--- src/main/kotlin/com/eatsfinder/domain/search/dto/PlaceSearchResponse.kt
+++ src/main/kotlin/com/eatsfinder/domain/search/dto/PlaceSearchResponse.kt
@@ -1,14 +1,32 @@
 package com.eatsfinder.domain.search.dto
 
+import com.eatsfinder.domain.place.model.Place
+import com.eatsfinder.domain.post.model.Post
+import com.eatsfinder.domain.starRating.model.StarRating
 import java.time.LocalDateTime
 
 data class PlaceSearchResponse(
-    val placeThumbnailUrl: String,
+    val postThumbnailUrl: String,
     val placeName: String,
     val roadAddress: String,
-    val starRating: String,
+    val starRating: Int,
     val category: String,
     val isBookmark: Boolean,
     val updatedAt: LocalDateTime,
     val likeCount: Int
-)
+) {
+    companion object {
+        fun from(place: Place, post: Post, star: StarRating, isBookmark: Boolean): PlaceSearchResponse {
+            return PlaceSearchResponse(
+                postThumbnailUrl = post.thumbnailUrl,
+                placeName = place.name,
+                roadAddress = place.roadAddress,
+                starRating = star.star,
+                category = place.categoryId.name,
+                isBookmark = isBookmark,
+                updatedAt = place.updatedAt,
+                likeCount = post.likeCount
+            )
+        }
+    }
+}
