c85834ab868a986a64ffe094899b7ab62e120bbb
rim109 <fla7722@naver.com>
2025-03-31 12:24:01 +0900

    ♻️ Refactor: import 삭제 (#117)

--- src/main/kotlin/com/eatsfinder/domain/follow/service/FollowServiceImpl.kt
+++ src/main/kotlin/com/eatsfinder/domain/follow/service/FollowServiceImpl.kt
@@ -8,7 +8,6 @@ import com.eatsfinder.global.exception.InvalidInputException
 import com.eatsfinder.global.exception.ModelNotFoundException
 import com.eatsfinder.global.exception.like.DefaultZeroException
 import com.eatsfinder.global.pagination.PaginationCursorItemsResponse
-import com.eatsfinder.global.pagination.PaginationItemsResponse
 import org.springframework.data.domain.PageRequest
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
