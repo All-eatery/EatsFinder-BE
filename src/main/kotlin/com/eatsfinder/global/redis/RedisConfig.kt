d5ff9d30cf3f910bc4ad97bce193763bb5d0c8b9
rim109 <fla7722@naver.com>
2025-05-18 00:28:40 +0900

    🍱 Add: REDIS Config 및 yml 파일 작성(#130)

---
+++ src/main/kotlin/com/eatsfinder/global/redis/RedisConfig.kt
@@ -0,0 +1,51 @@
+package com.eatsfinder.global.redis
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.cache.RedisCacheConfiguration
+import org.springframework.data.redis.cache.RedisCacheManager
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.StringRedisSerializer
+import java.time.Duration
+
+@EnableCaching
+@Configuration
+class RedisConfig(
+    @Value("\${spring.data.redis.host}") val host: String,
+    @Value("\${spring.data.redis.port}") val port: Int,
+) {
+    @Bean
+    fun lettuceConnectionFactory() = RedisStandaloneConfiguration().let {
+        it.hostName = host
+        it.port = port
+        it
+    }.let { LettuceConnectionFactory(it) }
+
+    @Bean
+    fun redisCacheManager(): RedisCacheManager {
+        return RedisCacheManager.builder(lettuceConnectionFactory())
+            .cacheDefaults(
+                RedisCacheConfiguration.defaultCacheConfig()
+                    .entryTtl(Duration.ofMinutes(10))
+            )
+            .build()
+    }
+
+
+    @Bean
+    fun redisTemplate(): RedisTemplate<*,*>{
+        val genericJackson2JsonRedisSerializer = GenericJackson2JsonRedisSerializer()
+        return RedisTemplate<Any, Any>().apply{
+            this.connectionFactory = lettuceConnectionFactory()
+            this.keySerializer = StringRedisSerializer()
+            this.valueSerializer = genericJackson2JsonRedisSerializer
+            this.hashKeySerializer = StringRedisSerializer()
+            this.hashValueSerializer = genericJackson2JsonRedisSerializer
+        }
+    }
+}
