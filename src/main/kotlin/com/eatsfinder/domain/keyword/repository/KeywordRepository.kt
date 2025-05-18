e0d8c2a6c81b1e90b16ec44fe662c03286266ecb
rim109 <fla7722@naver.com>
2025-05-18 21:21:03 +0900

    🧹 Remove: 필요없는 파일 삭제(#130)

--- src/main/kotlin/com/eatsfinder/domain/keyword/repository/KeywordRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.eatsfinder.domain.keyword.repository
-
-import com.eatsfinder.domain.keyword.model.KeywordLog
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface KeywordRepository: JpaRepository<KeywordLog, Long> {
-    fun findByKeyword(keyword: String): KeywordLog
-
-}
--- src/main/kotlin/com/eatsfinder/domain/keyword/service/KeywordService.kt
+++
@@ -1,7 +0,0 @@
-package com.eatsfinder.domain.keyword.service
-
-import com.eatsfinder.domain.keyword.dto.KeywordLogResponse
-
-interface KeywordService{
-    fun findKeywordLog(keyword: String): List<KeywordLogResponse>
-}
