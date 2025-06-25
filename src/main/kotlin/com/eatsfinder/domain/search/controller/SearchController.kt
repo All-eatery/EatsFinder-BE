44bdf333623b3a73a737c700846ef47effcc13fc
rim109 <fla7722@naver.com>
2025-06-25 20:56:53 +0900

    🐛 Fix: POST -> GET 로 수정 (#130)

--- src/main/kotlin/com/eatsfinder/domain/search/controller/SearchController.kt
+++ src/main/kotlin/com/eatsfinder/domain/search/controller/SearchController.kt
@@ -10,7 +10,6 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
@@ -19,7 +18,7 @@ class SearchController(
     private val searchService: SearchService
 ) {
     @Operation(summary = "검색하기")
-    @PostMapping("/search")
+    @GetMapping("/search")
     fun getSearchKeyword(
         @RequestParam keyword: String,
         @RequestParam filter: SearchFilter?,
