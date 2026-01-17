a9095c666b52c8aa243f5215e3571e0ad649384c
rim109 <fla7722@naver.com>
2026-01-17 23:30:19 +0900

    🍱 Add: 맛집 정보 res 값 추가(#131)

--- src/main/kotlin/com/eatsfinder/domain/place/service/PlaceInfoServiceImpl.kt
+++ src/main/kotlin/com/eatsfinder/domain/place/service/PlaceInfoServiceImpl.kt
@@ -3,14 +3,38 @@ package com.eatsfinder.domain.place.service
 import com.eatsfinder.domain.place.dto.PlaceInfoRequest
 import com.eatsfinder.domain.place.dto.PlaceInfoResponse
 import com.eatsfinder.domain.place.repository.PlaceRepository
+import com.eatsfinder.domain.post.model.Post
+import com.eatsfinder.domain.post.repository.PostRepository
 import org.springframework.stereotype.Service
 
 @Service
 class PlaceInfoServiceImpl(
-    private val placeRepository: PlaceRepository
+    private val placeRepository: PlaceRepository,
+    private val postRepository: PostRepository
 ): PlaceInfoService {
     override fun getPlaceMapInfo(req: PlaceInfoRequest): List<PlaceInfoResponse> {
-        val map = placeRepository.findByXBetweenAndYBetween(req.oa, req.ha, req.qa, req.pa)
-        return PlaceInfoResponse.from(map)
+
+        val places = placeRepository.findByXBetweenAndYBetween(
+            req.oa,
+            req.ha,
+            req.qa,
+            req.pa
+        )
+
+        if (places.isEmpty()) return emptyList()
+
+        val posts = postRepository.findByPlaceIdIn(places)
+
+        val postMap: Map<Long?, Post>? =
+            posts
+                ?.groupBy { it.placeId.id }
+                ?.mapValues { (_, postList) ->
+                    postList
+                        .maxByOrNull { it.createdAt }
+                }
+                ?.filterValues { it != null }
+                ?.mapValues { it.value!!}
+
+        return PlaceInfoResponse.from(places, postMap)
     }
 }
